860be93e72664b8f094aaef137e9b15421616a5f
TuongLe18110234 <55202649+TuongLe18110234@users.noreply.github.com>
2022-03-26 21:03:43 +0700

    Populate multiple fields

--- back-end/models/faculty.js
+++ back-end/models/faculty.js
@@ -5,7 +5,7 @@ const facultySchema = new mongoose.Schema({
     tenDonVi: {type: String, trim: true, required: [true, 'Nhập tên đơn vị']},
     moTa: {type: String},
     soDienThoai: {type: String, trim: true},
-    email: {type: String, unique: [true, 'Email đã tồn tại'], trim: true, required: [true, 'Nhập email quản lý']},
+    email: {type: String, unique: [true, 'Email đã tồn tại'], trim: true, required: [true, 'Nhập email đơn vị']},
     image: {type: String},
     loaiDonVi: {type: String, trim: true}, // DOAN_KHOA, DOAN_TRUONG, PHONG_BAN
     hienThi: {type: Boolean, default: false},
