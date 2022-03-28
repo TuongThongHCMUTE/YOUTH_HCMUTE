924b3f396880bde3b1a179dfadd10291977ce7bb
TuongLe18110234 <55202649+TuongLe18110234@users.noreply.github.com>
2022-03-29 00:10:39 +0700

    Add thongTinMacDinh for pricelist

--- back-end/models/priceList.js
+++ back-end/models/priceList.js
@@ -7,7 +7,12 @@ const priceListSchema = new mongoose.Schema({
     donViTinh: {type: String, trim: true}, // Tháng
     donGia: {type: Number, default: 0},
     hienThi: {type: Boolean, default: false}, // Chưa đóng đoàn phí, Đã đóng đoàn phí
-    matDinh: {type: Boolean, default: false},
+    thongTinMacDinh: {
+        ngayBatDau: {type: Date},
+        ngayKetThuc: {type: Date},
+        soLuong: {type: Number, default: 0},
+        thanhTien: {type: Number, default: 0},
+    },
     ghiChu: {type: String}, // Từ tháng 1/2022 tháng 12/2022
     createBy: {type: String},
     updateBy: {type: String}
