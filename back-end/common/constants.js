e85a8862323b9f7f57dbefc7e9242e42cfba1e0a
TuongLe18110234 <55202649+TuongLe18110234@users.noreply.github.com>
2022-03-29 00:09:39 +0700

    Caculate defaultBill

---
+++ back-end/common/constants.js
@@ -0,0 +1,30 @@
+exports.defaultBill = {
+    "tongTien": 50000,
+    // Sổ đoàn viên
+    "623a1299019af19b601f4084": {
+        "soLuong": 1,
+        "thanhTien": 0
+    },
+    // Đoàn phí
+    "623a12e8019af19b601f4089": {
+        "ngayBatDau": "2021-09-30T17:00:00.000+00:00",
+        "ngayKetThuc": "2022-09-30T17:00:00.000+00:00",
+        "soLuong": 12,
+        "thanhTien": 24000
+    },
+    // Truy thu Đoàn phí
+    "623a1341019af19b601f4092": {
+        "soLuong": 0,
+        "thanhTien": 0
+    },
+    // Kinh phí đóng góp công trình thanh niên
+    "623a136f019af19b601f4094": {
+        "soLuong": 12,
+        "thanhTien": 24000
+    },
+    // Kinh phí làm thẻ Hội viên hội sinh viên Việt Nam
+    "623a13b8019af19b601f4096": {
+        "soLuong": 1,
+        "thanhTien": 2000
+    }
+}
