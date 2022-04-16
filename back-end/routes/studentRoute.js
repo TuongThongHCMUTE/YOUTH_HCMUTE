9a09845e1173593efa36a12614edfe981bf115df
TuongLe18110234 <55202649+TuongLe18110234@users.noreply.github.com>
2022-04-17 00:36:33 +0700

    [BE] Change function name

--- back-end/routes/studentRoute.js
+++ back-end/routes/studentRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const { getAllStudents, createOneStudent, getOneStudent, getStudentInfo,
+const { getAllStudents, createOneStudent, getOneStudent, getStudentBarcode, getStudentInfo,
         updateOneStudent, deleteOneStudent}
     = require('../controllers/studentController')
 
@@ -8,7 +8,9 @@ const Router = express.Router()
 
 Router.route('/').get(getAllStudents).post(createOneStudent)
 
-Router.route('/thong-tin-sinh-vien/:maSoSV').get(getStudentInfo)
+Router.route('/thong-tin-barcode/:maSoSV').get(getStudentBarcode)
+
+Router.route('/thong-tin/:maSoSV').get(getStudentInfo)
 
 Router.route('/:id').get(getOneStudent).put(updateOneStudent).delete(deleteOneStudent)
 
