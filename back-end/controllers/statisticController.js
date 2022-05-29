e2ffb2f3034a68e160aada7a7840899086bdd09d
TuongLe18110234 <55202649+TuongLe18110234@users.noreply.github.com>
2022-05-30 00:08:21 +0700

    [BE] Add count account for statistic

--- back-end/controllers/statisticController.js
+++ back-end/controllers/statisticController.js
@@ -1,4 +1,5 @@
 const Student = require('../models/student')
+const Manager = require('../models/manager')
 const Faculty = require('../models/faculty')
 
 // Statistical data for landing page
@@ -15,6 +16,57 @@ exports.getDataForLandingPage = async (req, res, next) => {
                 totalUnionsMembers,
                 totalFaculties
             }
+        })
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+}
+
+// Count students by faculty
+exports.countStudentsByFaculty = async (req, res, next) => {
+    try {
+        const studentsByFaculty = await Student.aggregate([
+            {$group: {_id: '$donVi', count: {$sum: 1}}}
+        ])
+
+        const faculties = await Faculty.find({}).sort({tenDonVi: 1})
+
+        const countByFaculty = faculties.map(faculty => {
+            const dataCount = studentsByFaculty.find(x => x._id.toString() == faculty._id)
+            return {
+                _id: faculty._id,
+                tenDonVi: faculty.tenDonVi,
+                count: dataCount ? dataCount.count : 0
+            }
+        })
+
+        res.status(200).json({
+            status: 'success',
+            results: countByFaculty.length,
+            data: countByFaculty
+        })
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+}
+
+// Statistical data user
+exports.countUserByRole = async (req, res, next) => {
+    try {
+        const totalStudents = await Student.countDocuments({})
+
+        const managersByRole = await Manager.aggregate([
+            {$group: {_id: '$role', count: {$sum: 1}}}
+        ])
+
+        const usersByRole = [{_id: 'DOAN_VIEN', count: totalStudents}].concat(managersByRole)
+
+        res.status(200).json({
+            status: 'success',
+            results: usersByRole.length,
+            data: usersByRole
         })
     } catch (e) {
         console.log(e)
