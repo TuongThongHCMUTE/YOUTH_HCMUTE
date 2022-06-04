8ebbf7a0788388ad563f0919118076633acbab16
TuongLe18110234 <55202649+TuongLe18110234@users.noreply.github.com>
2022-06-04 15:46:49 +0700

    [BE] Add export excel for students and managers

--- back-end/routes/managerRoute.js
+++ back-end/routes/managerRoute.js
@@ -1,13 +1,15 @@
 const express = require('express')
 
 const { verifyToken } = require('../middleware/verifyToken')
-const { getAllManagers, createOneManager, getOneManager,
+const { getAllManagers, createOneManager, getOneManager, exportExcelAllManagers,
         updateOneManager, deleteOneManager }
     = require('../controllers/managerController')
 
 const Router = express.Router()
 
 Router.route('/').get(verifyToken, getAllManagers).post(verifyToken, createOneManager)
+
+Router.route('/xls').get(verifyToken, exportExcelAllManagers)
 
 Router.route('/:id').get(verifyToken, getOneManager).put(verifyToken, updateOneManager).delete(verifyToken, deleteOneManager)
 
