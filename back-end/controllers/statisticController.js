770ad78854627120f59aa062cc170f8baeac3637
TuongLe18110234 <55202649+TuongLe18110234@users.noreply.github.com>
2022-06-19 01:50:14 +0700

    [BE] Statis stuents, bills

--- back-end/controllers/statisticController.js
+++ back-end/controllers/statisticController.js
@@ -1,6 +1,7 @@
 const Student = require('../models/student')
 const Manager = require('../models/manager')
 const Faculty = require('../models/faculty')
+const Bill = require('../models/bill')
 
 // Statistical data for landing page
 exports.getDataForLandingPage = async (req, res, next) => {
@@ -73,3 +74,95 @@ exports.countUserByRole = async (req, res, next) => {
         next(e)
     }
 }
+
+// Students
+// Statistic students
+exports.statisticStudents = async (req, res, next) => {
+    try {
+        const totalStudents = await Student.countDocuments({})
+        const totalUnionsMembers = await Student.countDocuments({doanVien: true})
+        const totalYouth = totalStudents - totalUnionsMembers
+
+        const studentsByYear = await Student.aggregate([
+            {$group: {_id: '$khoaHoc', count: {$sum: 1}}}
+        ])
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                countData: {
+                    totalStudents,
+                    totalUnionsMembers,
+                    totalYouth
+                },
+                studentsByYear
+            }
+        })
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+}
+
+// Receipts
+exports.statisticBills = async (req, res, next) => {
+    try {
+        const totalUnionsMembers = await Student.countDocuments({doanVien: true})
+        const totalPaid = await Bill.countDocuments({trangThai: true})
+        const totalUnPaid = totalUnionsMembers - totalPaid
+
+        // Đếm số lượng và tổng tiền bill
+        const staticBill = await Bill.aggregate([
+            {
+                $group: {
+                    _id: null,
+                    count: {$sum: 1},
+                    total: { $sum: '$tongTien' }
+                }
+            }
+        ])
+
+        // Đếm số lượng và tổng tiền bill theo đơn vị
+        const staticBillByFaculty = await Bill.aggregate([
+            {
+                $group: {
+                    _id: '$donVi',
+                    count: {$sum: 1},
+                    total: { $sum: '$tongTien' }
+                }
+            }
+        ])
+
+        const faculties = await Faculty.find({}).sort({tenDonVi: 1})
+        const countByFaculty = faculties.map(faculty => {
+            const dataCount = staticBillByFaculty.find(x => x._id?.toString() == faculty._id)
+            return {
+                _id: faculty._id,
+                tenDonVi: faculty.tenDonVi,
+                count: dataCount ? dataCount.count : 0,
+                total: dataCount ? dataCount.total : 0
+            }
+        })
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                countData: {
+                    totalUnionsMembers,
+                    totalPaid,
+                    totalUnPaid
+                },
+                staticBill,
+                staticBillByFaculty: countByFaculty
+            }
+        })
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
+}
+
+
+// Events
+
+// Classes
--- back-end/routes/statisticRoute.js
+++ back-end/routes/statisticRoute.js
@@ -1,11 +1,16 @@
 const express = require('express')
 
-const { getDataForLandingPage, countStudentsByFaculty, countUserByRole}
+const { getDataForLandingPage, countStudentsByFaculty, countUserByRole, 
+        statisticStudents, statisticBills}
     = require('../controllers/statisticController')
 
 const Router = express.Router()
 
 Router.route('/trang-chu').get(getDataForLandingPage)
+
+Router.route('/sinh-vien').get(statisticStudents)
+
+Router.route('/hoa-don').get(statisticBills)
 
 Router.route('/sinh-vien-theo-don-vi').get(countStudentsByFaculty)
 
