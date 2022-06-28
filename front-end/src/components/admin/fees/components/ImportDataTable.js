be9d8a0ea955bc0fddbf441d8a6885deff284a3a
Đinh Bách Thông <87689155+thongdb@users.noreply.github.com>
2022-06-29 05:13:03 +0700

    [FE] Modify: Add border for data table

--- front-end/src/components/admin/fees/components/ImportDataTable.js
+++ front-end/src/components/admin/fees/components/ImportDataTable.js
@@ -6,35 +6,41 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import BorderTopCard from 'ui-component/cards/BorderTopCard';
 
 export default function BasicTable({ data }) {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>STT</TableCell>
-            <TableCell>Họ và tên</TableCell>
-            <TableCell>Mã số sinh viên</TableCell>
-            <TableCell>Đoàn phí</TableCell>
-            <TableCell>Công trình thanh niên</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row) => (
-            <TableRow
-              key={row.stt}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell>{row.stt}</TableCell>
-              <TableCell>{row.hoVaTen}</TableCell>
-              <TableCell>{row.maSoSV}</TableCell>
-              <TableCell>{row.doanPhi}</TableCell>
-              <TableCell>{row.cttn}</TableCell>
+    <BorderTopCard
+        borderColor="var(--color-grey-300"
+        topColor="var(--color-primary-400)"
+    >
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>STT</TableCell>
+              <TableCell>Họ và tên</TableCell>
+              <TableCell>Mã số sinh viên</TableCell>
+              <TableCell>Đoàn phí</TableCell>
+              <TableCell>Công trình thanh niên</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.stt}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{row.stt}</TableCell>
+                <TableCell>{row.hoVaTen}</TableCell>
+                <TableCell>{row.maSoSV}</TableCell>
+                <TableCell>{row.doanPhi}</TableCell>
+                <TableCell>{row.cttn}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </BorderTopCard>
   );
 }
--- front-end/src/components/admin/fees/components/ImportErrorsTable.js
+++ front-end/src/components/admin/fees/components/ImportErrorsTable.js
@@ -6,29 +6,35 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import BorderTopCard from 'ui-component/cards/BorderTopCard';
 
 export default function BasicTable({ data }) {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>key</TableCell>
-            <TableCell>Lỗi</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.length > 0 && data.map((row) => (
-            <TableRow
-              key={row.stt}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell>{row.key}</TableCell>
-              <TableCell>{row.message}</TableCell>
+    <BorderTopCard
+        borderColor="var(--color-grey-300"
+        topColor="var(--color-primary-400)"
+    >
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>key</TableCell>
+              <TableCell>Lỗi</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data.length > 0 && data.map((row) => (
+              <TableRow
+                key={row.stt}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{row.key}</TableCell>
+                <TableCell>{row.message}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </BorderTopCard>
   );
 }
