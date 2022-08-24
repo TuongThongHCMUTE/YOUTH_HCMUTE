ae082e52b3a602eaf4a3336b304655121e0ec2e9
thongdb <dinhbachthong@gmail.com>
2022-08-25 02:07:36 +0700

    [FE] init refactor FE

---
+++ client/src/components/landing/index.js
@@ -0,0 +1,13 @@
+// Node Modules ============================================================ //
+import React from 'react';
+
+// ===========================|| LANDING PAGE ||============================ //
+const Landing = () => {
+  return (
+    <div >
+      Landing Page
+    </div>
+  )
+};
+
+export default Landing;
