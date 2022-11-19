333629d737e5403dc8f0f84e66fa96523ff1c3d4
Đinh Bách Thông <thongdb.dev@gmail.com>
2022-11-19 13:20:22 +0700

    YOUTH-22: Improving Performance for Introduction

--- client/src/components/landing/components/Introduction.js
+++ client/src/components/landing/components/Introduction.js
@@ -4,72 +4,71 @@ import React from 'react';
 import styles from './Introduction.module.scss';
 // Material UI ============================================================= //
 import { Button, Grid } from '@mui/material';
-import FitScreenOutlinedIcon from '@mui/icons-material/FitScreenOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined';
-import GetAppOutlinedIcon from '@mui/icons-material/GetAppOutlined';
 // Assets ================================================================== //
 import image from 'assets/images/hero-img.png';
+// Constants =============================================================== //
+import {
+  LANDING_PAGE_INTRODUCTION,
+  LANDING_PAGE_FEATURE
+} from 'helpers/landing';
 
 // ============================|| INTRODUCTION ||=========================== //
 const Introduction = () => {
+  const pageIntroduction = (
+    <React.Fragment>
+      <div className={styles.Info}>
+        <h1>{LANDING_PAGE_INTRODUCTION.header}</h1>
+        <h3>{LANDING_PAGE_INTRODUCTION.subHeader}</h3>
+        <p>{LANDING_PAGE_INTRODUCTION.description}</p>
+        <div className={styles.ButtonWrapper}>
+          <Button className={styles.OutlinedButton}>Khám phá ngay</Button>
+        </div>
+      </div>
+      <div className={styles.ImageContainer}>
+        <img className={styles.Image} src={image} alt="" />
+      </div>
+    </React.Fragment>
+  );
+
+  const pageFeatures = (
+    <React.Fragment>
+      <div className={styles.Title}>
+        <h2>{LANDING_PAGE_FEATURE.header}</h2>
+        <p>{LANDING_PAGE_FEATURE.quote}</p>
+      </div>
+      <ul className={styles.Items}>
+        {LANDING_PAGE_FEATURE.features.map(feature => {
+          const Icon = feature.icon;
+          return (
+            <li key={feature.id}>
+              <div className={styles.Item}>
+                <div className={styles.IconWrapper}>
+                  <Icon className={styles.Icon} />
+                </div>
+                <h3>{feature.name}</h3>
+                <p>{feature.description}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </React.Fragment>
+  );
+
   return (
     <div id="introduction">
       <Grid container className={styles.Introduction}>
         <Grid item xs={10} className={styles.Content}>
-          <div className={styles.Info}>
-            <h1>TRANG THÔNG TIN QUẢN LÝ ĐOÀN VIÊN</h1>
-            <h3>Trường Đại học Sư phạm Kỹ Thuật TP. Hồ Chí Minh</h3>
-            <p>Website cung cấp các công cụ tiện ích hỗ trợ Đoàn trường, Đoàn khoa quản lý thông tin, hồ sơ đoàn vụ của sinh viên trường Đại học Sư phạm Kỹ thuật thành phố Hồ Chí Minh. Đồng thời website có thể theo dõi thông tin về việc nộp sổ đoàn, nhận xét trong quá trình sinh hoạt Đoàn tại trường và đóng Đoàn phí.</p>
-            <div className={styles.ButtonWrapper}>
-              <Button className={styles.OutlinedButton}>Khám phá ngay</Button>
-            </div>
-          </div>
-          <div className={styles.ImageContainer}>
-            <img className={styles.Image} src={image} alt='' />
-          </div>
+          {pageIntroduction}
         </Grid>
-      </Grid>  
+      </Grid>
       <Grid id="feature" container className={styles.FeatureSection}>
         <Grid item xs={10} className={styles.Features}>
-          <div className={styles.Title}>
-            <h2>Chức năng chính</h2>
-            <p>Cung cấp các tính năng hỗ trợ quản lý thông tin đoàn viên, hồ sơ, đoàn vụ</p>
-          </div>
-          <div className={styles.Items}>
-            <div className={styles.Item}>
-              <div className={styles.IconWrapper}>
-                <FitScreenOutlinedIcon className={styles.Icon} />
-              </div>
-              <h3>Thu sổ đoàn</h3>
-              <p>Nhập mô tả</p>
-            </div>
-            <div className={styles.Item}>
-              <div className={styles.IconWrapper}>
-                <SearchOutlinedIcon className={styles.Icon} />
-              </div>
-              <h3>Tra cứu thông tin đoàn viên</h3>
-              <p>Nhập mô tả</p>
-            </div>
-            <div className={styles.Item}>
-              <div className={styles.IconWrapper}>
-                <DriveFileRenameOutlineOutlinedIcon className={styles.Icon} />
-              </div>
-              <h3>Phê sổ đoàn trực tuyến</h3>
-              <p>Nhập mô tả</p>
-            </div>
-            <div className={styles.Item}>
-              <div className={styles.IconWrapper}>
-                <GetAppOutlinedIcon className={styles.Icon} />
-              </div>
-              <h3>Đăng ký rút sổ đoàn</h3>
-              <p>Nhập mô tả</p>
-            </div>
-          </div>
+          {pageFeatures}
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
 export default Introduction;
--- client/src/helpers/landing.js
+++ client/src/helpers/landing.js
@@ -1,3 +1,8 @@
+import FitScreenOutlinedIcon from '@mui/icons-material/FitScreenOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import DriveFileRenameOutlineOutlinedIcon from '@mui/icons-material/DriveFileRenameOutlineOutlined';
+import GetAppOutlinedIcon from '@mui/icons-material/GetAppOutlined';
+
 export const LANDING_PAGE_SECTIONS = [
   {
     id: 'header',
@@ -24,3 +29,45 @@ export const LANDING_PAGE_SECTIONS = [
     display: 'Liên hệ'
   }
 ];
+
+export const LANDING_PAGE_INTRODUCTION = {
+  header: 'TRANG THÔNG TIN QUẢN LÝ ĐOÀN VIÊN',
+  subHeader: 'Trường Đại học Sư phạm Kỹ Thuật TP. Hồ Chí Minh',
+  description:
+    'Website cung cấp các công cụ tiện ích hỗ trợ Đoàn trường, Đoàn khoa ' +
+    'quản lý thông tin, hồ sơ đoàn vụ của sinh viên ' + 
+    'trường Đại học Sư phạm Kỹ thuật thành phố Hồ Chí Minh. ' + 
+    'Đồng thời website có thể theo dõi thông tin về việc ' + 
+    'nộp sổ đoàn, nhận xét trong quá trình sinh hoạt Đoàn tại trường và đóng Đoàn phí.',
+};
+
+export const LANDING_PAGE_FEATURE = {
+  header: 'Chức năng chính',
+  quote: 'Cung cấp các tính năng hỗ trợ quản lý thông tin đoàn viên, hồ sơ, đoàn vụ',
+  features: [
+    {
+      id: 'thu-so-doan',
+      name: 'Thu sổ đoàn',
+      description: 'Mô tả chức năng',
+      icon: FitScreenOutlinedIcon,
+    },
+    {
+      id: 'tra-cuu-thong-tin',
+      name: 'Tra cứu thông tin đoàn viên',
+      description: 'Mô tả chức năng',
+      icon: SearchOutlinedIcon,
+    },
+    {
+      id: 'phe-so-doan',
+      name: 'Phê sổ đoàn trực tuyến',
+      description: 'Mô tả chức năng',
+      icon: DriveFileRenameOutlineOutlinedIcon,
+    },
+    {
+      id: 'rut-so-doan',
+      name: 'Đăng ký rút sổ đoàn',
+      description: 'Mô tả chức năng',
+      icon: GetAppOutlinedIcon,
+    },
+  ],
+}
